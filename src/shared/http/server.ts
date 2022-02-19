3517a1c416df603242b48a98a899df2ea6d82f3e
Uilton Anunciacao <uilton.anunciacao@acpgroup.com.br>
2022-02-19 06:51:35 -0300

    Configurando banco de dados e typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 

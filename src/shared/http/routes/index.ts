5ff7beeda984c64e2ebf91bda2477291816e007d
Uilton Anunciacao <uilton.anunciacao@acpgroup.com.br>
2022-03-15 21:20:46 -0300

    Implementação de rotinas de produtos

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'API online' });
-});
+routes.use('/products', productsRouter);
 
 export default routes;

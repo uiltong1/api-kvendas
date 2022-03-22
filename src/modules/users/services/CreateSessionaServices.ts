62a8c683274d52f23682c7ecf9639831cf492c7c
Uilton Anunciacao <uilton.anunciacao@acpgroup.com.br>
2022-03-21 22:31:34 -0300

    Configuração de autenticação

--- src/modules/users/services/CreateSessionaServices.ts
+++ src/modules/users/services/CreateSessionaServices.ts
@@ -1,6 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '@config/authConfig';
 import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '../typeorm/repositories/UserRepository';
 import User from '../typeorm/entities/User';
@@ -31,9 +32,9 @@ class CreateSessionsServices {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    const token = sign({}, '123456789', {
+    const token = sign({}, authConfig.jwt.secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn: authConfig.jwt.expiresIn,
     });
 
     return {
